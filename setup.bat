a420a4c57560fafc607265f5d59e20b4035a4f94
Nate Merrill <nmerrill@udel.edu>
2017-12-27 10:58:46 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
-mkdir Decom_tools/data
-mkdir Decom_tools/databases
-mkdir Matlab_tools/data
-mkdir Matlab_tools/DBD_XLS
-mkdir Matlab_tools/DBD_CSVs
-mkdir Matlab_tools/DBD_XML
-mkdir Decom_tools/database_CSVs
+mkdir Decom_tools\data
+mkdir Decom_tools\databases
+mkdir Matlab_tools\data
+mkdir Matlab_tools\DBD_XLS
+mkdir Matlab_tools\DBD_CSVs
+mkdir Matlab_tools\DBD_XML
+mkdir Decom_tools\database_CSVs
